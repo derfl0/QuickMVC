51408f52b15e256409585a6684e3bf7e4ea1b537
Florian Bieringer <florian.bieringer@uni-passau.de>
2016-01-18 17:28:12 +0100

    Fix meta handling and delete

--- lib/QuickORM.php
+++ lib/QuickORM.php
@@ -3,68 +3,72 @@
 class QuickORM
 {
     protected static $meta;
-
     protected static $storage;
 
     public static function findBy($key, $value)
     {
         $stmt = QuickDB::get()->prepare("SELECT * FROM " . static::DB_TABLE . " WHERE $key=?");
         $stmt->execute(array($value));
-        self::setMeta($stmt);
+        self::setMeta(get_called_class(), $stmt);
         return $stmt->fetchObject(get_called_class());
     }
 
     public function store()
     {
-        $meta = static::getMeta();
+        $meta = static::getMeta(get_called_class());
 
         // Prepare SQL query
         $prepared = substr(str_repeat("?,", count($meta)), 0, -1);
         $stmt = QuickDB::get()->prepare("REPLACE INTO " . static::DB_TABLE . " VALUES ($prepared)");
-
         // Prepare values
         foreach ($meta as $m) {
             $params[] = $this->$m['name'];
         }
-
         // Execute
         $stmt->execute($params);
     }
 
-    private static function setMeta($stmt)
+    private static function setMeta($class, $stmt)
     {
-        if (!static::$meta) {
+        if (!static::$meta[$class]) {
             while ($meta = $stmt->getColumnMeta($i++)) {
-                static::$meta[] = $meta;
+                static::$meta[$class][] = $meta;
             }
         }
     }
 
-    private static function getMeta()
+    private static function getMeta($class)
     {
-        if (!static::$meta) {
-
+        if (!static::$meta[$class]) {
             // Fetch meta
             $stmt = QuickDB::get()->prepare("SHOW COLUMNS FROM " . static::DB_TABLE);
             $stmt->execute();
             while ($col = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                static::$meta[] = array('name' => $col['Field']);
+                static::$meta[$class][] = array('name' => $col['Field']);
             }
         }
-        return static::$meta;
+        return static::$meta[$class];
     }
 
-    public static function find($where = '1=1', $params = array()) {
+    public static function find($where = '1=1', $params = array())
+    {
+        if (!is_array($params)) {
+            $params = array($params);
+        }
+
         $stmt = QuickDB::get()->prepare("SELECT * FROM " . static::DB_TABLE . " WHERE $where");
         $stmt->execute($params);
         static::$storage = $stmt;
+        return $stmt;
     }
 
-    public static function fetch() {
+    public static function fetch()
+    {
         return static::$storage->fetchObject(get_called_class());
     }
 
-    public static function fetchAll() {
+    public static function fetchAll()
+    {
         while ($obj = static::fetch()) {
             $result[] = $obj;
         }
@@ -83,5 +87,14 @@ class QuickORM
         $object = new static();
         $object->setData($data);
         return $object;
+    }
+
+    public static function deleteBy($where = '1=1', $params = array())
+    {
+        if (!is_array($params)) {
+            $params = array($params);
+        }
+        $stmt = QuickDB::get()->prepare("DELETE FROM " . static::DB_TABLE . " WHERE $where");
+        $stmt->execute($params);
     }
 }
