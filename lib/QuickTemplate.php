9600607f3d96b7fc55017b7216308874691d19f2
Florian Bieringer <florian.bieringer@uni-passau.de>
2016-01-18 17:26:06 +0100

    Skript and Style options in layout

--- lib/QuickTemplate.php
+++ lib/QuickTemplate.php
@@ -1,21 +1,39 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: intelec
- * Date: 15.01.16
- * Time: 13:51
- */
 class QuickTemplate
 {
     private static $template = 'default';
+    private static $scripts = array();
+    private static $style = array();
 
     public static function setTemplate($template) {
         self::$template = $template;
     }
-
     public static function render() {
         global $controller;
         include QuickConfig::$rootpath.'/templates/'.self::$template.'.php';
+    }
+
+    public static function addScript($script) {
+        self::$scripts[] = $script;
+    }
+
+    public static function addStyle($style) {
+        self::$style[] = $style;
+    }
+
+    public static function getHead() {
+
+        // Add all stylesheets
+        foreach (self::$styles as $style) {
+            $head .= '<link rel="stylesheet" href="'.$style.'">';
+        }
+
+        // Add all scripts
+        foreach (self::$scripts as $script) {
+            $head .= ' <script src="'.$script.'"></script> ';
+        }
+
+        return $head;
     }
 }
