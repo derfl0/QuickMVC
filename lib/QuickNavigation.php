b59d2057e16e3687cc8fc0bb58110db37a237bf8
Florian Bieringer <florian.bieringer@uni-passau.de>
2016-01-20 10:43:09 +0100

    Add QuickNavigation

---
+++ lib/QuickNavigation.php
@@ -0,0 +1,56 @@
+<?php
+
+class QuickNavigation
+{
+    private static $navigation;
+    private static $active = false;
+    private $_subnavigation = array();
+    private $_link;
+    private $_text;
+
+    public static function get()
+    {
+        if (!self::$navigation) {
+            self::$navigation = new self;
+        }
+        return self::$navigation;
+    }
+
+    public function add($link, $text, $name = null)
+    {
+        $nav = new self();
+        $nav->set($link, $text);
+        $this->_subnavigation[$name] = $nav;
+
+        // Set member var
+        if ($name) {
+            $this->$name = $this->_subnavigation[$name];
+        }
+    }
+
+    public function set($link, $text)
+    {
+        $this->_link = $link;
+        $this->_text = $text;
+    }
+
+    public function addRoute($route, $text, $name = null)
+    {
+        $this->add(QuickURL::generate($route), $text, $name);
+    }
+
+    public function render()
+    {
+        if ($this->_link) {
+            $response = "<li><a href='{$this->_link}'>{$this->_text}</a></li>";
+        }
+        if ($this->_subnavigation) {
+            $response .= "<ul>";
+            foreach ($this->_subnavigation as $sub) {
+                $response .= $sub->render();
+            }
+            $response .= "</ul>";
+        }
+        return $response;
+    }
+}
